add5b0a2c95a5d03dbd26416ce07f5e0910a145b
conorburke <cjburke89@gmail.com>
2019-11-01 16:11:59 +0900

    add kicker and defense api routes, remove excessive logging

--- api/models.go
+++ api/models.go
@@ -1,64 +1,80 @@
 package main
 
+type fantasyEntity interface {
+	getTeam() string
+}
+
 type offensePlayer struct {
-	FirstName    string
-	LastName     string
-	Team         string
-	Bye          string
-	Pos          string
-	PassYards    int
-	Int          int
-	RushYds      int
-	Catch        int
-	RecYds       int
-	RegTDs       int
-	Bonus        int
-	Dynst        int
-	PointsESPN   int
-	VBDReg       int
-	PointsPPR    int
-	PPRVBD       int
-	PointsTD     int
-	VBDTD        int
-	PointsTwoQB  int
-	VBDTwoQB     int
-	PointsCustom int
-	VBDCustom    int
+	FirstName    string `json:"firstName"`
+	LastName     string `json:"lastName"`
+	Team         string `json:"team"`
+	Bye          string `json:"bye"`
+	Pos          string `json:"position"`
+	PassYards    int `json:"passYards"`
+	Int          int `json:"interceptions"`
+	RushYds      int `json:"rushYards"`
+	Catch        int `json:"catches"`
+	RecYds       int `json:"receivingYards"`
+	RegTDs       int `json:"regYards"`
+	Bonus        int `json:"bonus"`
+	Dynst        int `json:"dynst"`
+	PointsESPN   int `json:"pointsEspn"`
+	VBDReg       int `json:"vbdReg"`
+	PointsPPR    int `json:"pointsPpr"`
+	PPRVBD       int `json:"pprVbd"`
+	PointsTD     int `json:"pointsTd"`
+	VBDTD        int `json:"vbdTd"`
+	PointsTwoQB  int `json:"pointsTwoQb"`
+	VBDTwoQB     int `json:"vbdTwoQb"`
+	PointsCustom int `json:"pointsCustom"`
+	VBDCustom    int `json:"vbdCustom"`
 }
 
 type kicker struct {
-	FirstName    string
-	LastName     string
-	Team         string
-	Bye          string
-	Pos          string
-	FG1to39      int
-	FG40to49     int
-	FG50Plus     int
-	XP           int
-	ESPN         int
-	VBDReg       int
-	PointsPPR    int
-	PPRVBD       int
-	PointsTD     int
-	VBDTD        int
-	PointsTwoQB  int
-	VBDTwoQB     int
-	PointsCustom int
-	VBDCustom    int
+	FirstName    string `json:"firstName"`
+	LastName     string `json:"lastName"`
+	Team         string `json:"team"`
+	Bye          string `json:"bye"`
+	Pos          string `json:"position"`
+	FG1to39      int `json:"fg1to39"`
+	FG40to49     int `json:"fg40to49"`
+	FG50Plus     int `json:"fg50Plus"`
+	XP           int `json:"xp"`
+	ESPN         int `json:"espn"`
+	VBDReg       int `json:"vbdReg"`
+	PointsPPR    int `json:"pointsPpr"`
+	PPRVBD       int `json:"pprVbd"`
+	PointsTD     int `json:"pointsTd"`
+	VBDTD        int `json:"vbdTd"`
+	PointsTwoQB  int `json:"pointsTwoQb"`
+	VBDTwoQB     int `json:"vgdTwoQb"`
+	PointsCustom int `json:"pointsCustom"`
+	VBDCustom    int `json:"vbdCustom"`
 }
 
 type defense struct {
-	Team        string
-	Bye         string
-	RegScore    int
-	VBDReg      int
-	PPRScore    int
-	VBDPPR      int
-	TDScore     int
-	VBDTD       int
-	TwoQBScore  int
-	VBDTwoQB    int
-	CustomScore int
-	VBDCustom   int
+	Team        string `json:"team"`
+	Bye         string `json:"bye"`
+	RegScore    int `json:"regScore"`
+	VBDReg      int `json:"vbdReg"`
+	PPRScore    int `json:"pprScore"`
+	VBDPPR      int `json:"vbdPpr"`
+	TDScore     int `json:"tdScore"`
+	VBDTD       int `json:"vbdTd"`
+	TwoQBScore  int `json:"twoQbScore"`
+	VBDTwoQB    int `json:"vbdTwoQb"`
+	CustomScore int `json:"customScore"`
+	VBDCustom   int `json:"vbdCustom"`
+}
+
+func (o offensePlayer) getTeam() string {
+	return o.Team
+}
+
+func (k kicker) getTeam() string {
+	return k.Team
+}
+
+func (d defense) getTeam() string {
+	return d.Team
 }
--- api/routes.go
+++ api/routes.go
@@ -13,36 +13,77 @@ func indexHandler(res http.ResponseWriter, req *http.Request, _ httprouter.Param
 	res.Write([]byte("Leave Me Here"))
 }
 
-func getOffensePlayers(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	players, err := selectOffensePlayers("")
-	marshallAndSend(players, err, w)
+func getOffensePlayers(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	players, err := selectOffensePlayers(ps.ByName("position"))
+	marshallAndSendOffense(players, err, w)
 }
 
-func getQuarterbacks(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	players, err := selectOffensePlayers("qb")
-	marshallAndSend(players, err, w)
+// func getOffensePlayers(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+// 	players, err := selectOffensePlayers("")
+// 	marshallAndSendOffense(players, err, w)
+// }
+
+// func getQuarterbacks(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+// 	players, err := selectOffensePlayers("qb")
+// 	marshallAndSendOffense(players, err, w)
+// }
+
+// func getRunningBacks(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+// 	players, err := selectOffensePlayers("rb")
+// 	marshallAndSendOffense(players, err, w)
+// }
+
+// func getWideReceivers(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+// 	players, err := selectOffensePlayers("wr")
+// 	marshallAndSendOffense(players, err, w)
+// }
+
+// func getTightEnds(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+// 	players, err := selectOffensePlayers("te")
+// 	marshallAndSendOffense(players, err, w)
+// }
+
+func getKickers(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	kickers, err := selectKickers()
+	marshallAndSendKicker(kickers, err, w)
 }
 
-func getRunningBacks(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	players, err := selectOffensePlayers("rb")
-	marshallAndSend(players, err, w)
+func getDefenses(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	defenses, err := selectDefenses()
+	marshallAndSendDefense(defenses, err, w)
 }
 
-func getWideReceivers(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	players, err := selectOffensePlayers("wr")
-	marshallAndSend(players, err, w)
-}
-
-func getTightEnds(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	players, err := selectOffensePlayers("te")
-	marshallAndSend(players, err, w)
-}
-
-func marshallAndSend(players []offensePlayer, err error, w http.ResponseWriter) {
+func marshallAndSendOffense(players []offensePlayer, err error, w http.ResponseWriter) {
 	if err != nil {
 		panic(err)
 	}
 	js, err := json.Marshal(players)
+	if err != nil {
+		fmt.Println(err)
+	}
+	os.Stdout.Write(js)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
+
+func marshallAndSendKicker(kickers []kicker, err error, w http.ResponseWriter) {
+	if err != nil {
+		panic(err)
+	}
+	js, err := json.Marshal(kickers)
+	if err != nil {
+		fmt.Println(err)
+	}
+	os.Stdout.Write(js)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
+
+func marshallAndSendDefense(defenses []defense, err error, w http.ResponseWriter) {
+	if err != nil {
+		panic(err)
+	}
+	js, err := json.Marshal(defenses)
 	if err != nil {
 		fmt.Println(err)
 	}
